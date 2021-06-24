130986c2e600ee4aadf8bc9c50166cef539b6d20
Avi Schwarzschild <avi1@math.umd.edu>
2021-06-24 15:54:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,13 @@ from io import open
 
 here = path.abspath(path.dirname(__file__))
 sys.path.insert(0, path.join(here, 'easy-to-hard-data'))
-from version import __version__
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-print('version')
-print(__version__)
-
 setup(name='easy_to_hard_data',
-      version="v0.1.5",
+      version="v0.1.6",
       description='Easy to Hard Data',
       url='https://aks2203.github.io/easy-to-hard-data',
       author='Avi Schwarzschild',
